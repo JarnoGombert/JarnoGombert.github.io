451feac70c5aa395a585c8c9a9ab7d48bcff4445
Jarno Gombert <jarnogombert@gmail.com>
2022-06-10 17:45:59 +0200

    All files

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loverzz</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+    <body>
+        <footer>
+            <ul>
+                <li>
+                    <img src="images/duyulmamış-rap-sözleri-300x300.jpg" alt="Logo"><span> © 2022 JarnoG, Inc.</span>
+                </li>
+            </ul>
+            <ul></ul>
+        </footer> 
+    </body>
+</html>
